191b8c76b5a375741139396eab92c48e452a4063
Devfrank94 <francescomurro94@gmail.com>
2023-03-23 17:05:25 +0100

    partial commit

---
+++ Mail/js/script.js
@@ -0,0 +1,28 @@
+//1. Genero array con all'interno delle mail fittizie
+//2. Attraverso il prompt chiedo all'untente di inserire una mail
+//3. Creo un ciclo for che verichi se la mail inserita è strettamente uguale all'elemento interno all'array.
+//4. Stampo il messaggio in pagina.
+
+const arrayEmail = ['ciao@gmail.com',
+                'frank@libero.it',
+                'john@libero.it',
+                'doe@gmail.com',
+                'lorem@gmail.com',
+                'red@libero.it',
+                'green@gmail.com',];
+
+  console.log(arrayEmail);
+
+const userEmail = prompt('Inserisci una mail da verificare')
+
+let verified = false;
+
+for(let i = 0; i < arrayEmail.length; i++){
+  console.log(arrayEmail[i]);
+
+  
+
+
+
+
+}
